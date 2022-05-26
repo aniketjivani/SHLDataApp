7d24886f04972dddfc9af934b84c2c9b8af67cd2
Aniket Jivani <ajivani@umich.edu>
2022-05-26 03:10:27 -0400

    add print statement in main

--- main.jl
+++ main.jl
@@ -1,6 +1,8 @@
 
+
 include("./dashboard.jl")
 
+println("all done here")
 port = something(tryparse(Int, get(ARGS, 1, "")), tryparse(Int, get(ENV, "PORT", "")), 8080)
 
 run_server(app, "0.0.0.0", port)
