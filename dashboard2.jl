714c41e7768c0197c14bf40ca518e25b681b1e11
Aniket Jivani <ajivani@umich.edu>
2022-05-31 01:53:51 -0400

    add more plots and cleanup

--- dashboard2.jl
+++ dashboard2.jl
@@ -43,6 +43,103 @@ function getStartEndTimes(filePath)
     shlStartIdx, shlEndIdx, obsStartIdx, obsEndIdx = getStartEndTimes(d)
 end
 
+function loadRMSEData(filePath)
+
+    d = load(filePath)
+
+    UrOptRMSE = d["rmseUr"]
+    NpOptRMSE = d["rmseNp"]
+    BzOptRMSE = d["rmseBz"]
+
+    return UrOptRMSE, NpOptRMSE, BzOptRMSE
+end
+
+"""
+Make heatmap of quantity, for eg RMSE, OTS, etc which varies with latitude and longitude.
+Typically quantity will be defined for multiple runs, so make heatmap for specified `plotIdx`
+"""
+function plotLatLonHeatmap(latVals, lonVals, latLonQoI, plotIdx; 
+                           lat=-4,
+                           lon=172,
+                        qoi = "Ur", 
+                        clims=(0, 300), 
+                        palette=cgrad(:RdBu_9, rev=true), 
+                        colorbar=true, 
+                           ylabel="Latitude")
+    plotly()
+    p = heatmap(lonVals, latVals, latLonQoI[:, :, plotIdx], clim=clims,
+	        c=palette)
+    titleText = "QoI: " * qoi * " Run $(runsToKeep[plotIdx] + 20)"
+    plot!(xlabel="Longitude", ylabel=ylabel, title=titleText)
+    plot!(xlims=(170, 190))
+    plot!(ylims=(-10, 20))
+    plot!(colorbar=colorbar)
+    plot!(legend=false)
+    scatter!([180], [7], marker=(:circle, 6, :blue), label="")
+    scatter!([lon],[lat])
+             # annotations = (pLon + 0.6, pLat - 0.6, Plots.text("($pLon, $pLat)", :center, :blue, 10))
+    plot!(size=(500, 400))
+
+    dataFig = Plots.plotly_series(p)
+    dataFig[1][:x] = string.(lonVals)
+    dataFig[1][:y] = string.(latVals)
+    dataFig[1][:type] = "heatmap"
+    dataFig[1][:z] = [latLonQoI[i, :, plotIdx] for i in 1:length(latVals)]
+    layoutFig = Plots.plotly_layout(p)
+
+    # fix overlapping of axis title and tick labels
+   layoutFig[:xaxis][:standoff] = 20
+   layoutFig[:yaxis][:standoff] = 20
+
+   layoutFig[:title] = Dict{Symbol, Any}(:text=>titleText, :x=>0.5, :xanchor=>"center", :xref=>"paper")
+    delete!(layoutFig, :annotations)
+    layoutFig[:margin] = Dict{Symbol, Any}(:l => 40, :b=> 40, :r=>0, :t=>30) 
+    figure = (data = dataFig, layout = layoutFig)
+    return figure
+end
+
+"""
+Make plot of given run at given latitude and longitude and also plot the 
+corresponding shifted version based on specified shifts.
+"""
+function plotShiftedQoI(sim, obs, ots, plotIdx, times, lat, lon, latVec, lonVec;  											
+                        plot_obs=true, 
+                        palette=:Dark2_8,
+                        tickInterval=12,
+                        tickFormat="dd-mm",
+                        linealpha=0.6,
+                        xlabel="",
+                        ylabel="Ur",
+                        ylims=(200, 800),
+                        legend=true,
+                        )
+
+    pLatLon = latLonPlotAllSamples(sim, obs, plotIdx, times, lat, lon, latVec, lonVec;  											
+                                plot_obs=plot_obs, 
+                                palette=palette,
+                                tickInterval=tickInterval,
+                                tickFormat=tickFormat,
+                                linealpha=linealpha,
+                                xlabel=xlabel,
+                                ylabel=ylabel,
+                                ylims=ylims,
+                                legend=legend
+                                )
+    
+    latIndex = findall(x -> x==lat, latVec)[1]
+    lonIndex = findall(x -> x==lon, lonVec)[1]
+                            
+    chosenOTS = ots[latIndex, lonIndex, plotIdx]
+    qoiShifted = lag(sim[latIndex, lonIndex, :, plotIdx], chosenOTS)
+    plot!(pLatLon,
+        qoiShifted, 
+        line=(:blue, 2),
+        label = "Shifted, OTS = $(chosenOTS)"
+        )
+    
+    # convert to plotly figure!!!!!
+end
+
 
 """
 Take in shl data, obs data at all latitudes for a given background (defaults to 4 for CR2154), plot that, mark map time and shock arrival time. also highlight latitudes based on optional argument (defaults to earth and -4)
@@ -50,7 +147,6 @@ Take in shl data, obs data at all latitudes for a given background (defaults to 
 function plotSHLData(simData, obsData)
     return nothing
 end
-
 
 """
 Take in a filepath, QoI to plot and optional args for bg run and lats to highlight, and call main plotting function (plotSHLData).
@@ -140,7 +236,7 @@ function makeLatPlots(filePath;
     #     plot!(title = "Highlighted Lat = $(highlightLats)", titlelocation=:center)
     # end
 
-    plot!(size=(800, 600))
+    plot!(size=(800, 630))
     # return p
 
     dataFig = Plots.plotly_series(p)
@@ -165,12 +261,61 @@ function makeLatPlots(filePath;
     # layoutFig[:annotations][1][:xanchor]=:right
     # layoutFig[:annotations][1][:align] = "center"
     # layoutFig[:title] = Dict{Symbol, Any}(:xref=> "paper", :xanchor=>"center")
-    
+    delete!(layoutFig, :annotations)
     layoutFig[:margin] = Dict{Symbol, Any}(:l => 40, :b=> 40, :r=>0, :t=>30) 
     figure = (data = dataFig, layout = layoutFig)
     return figure
 end
 
+"""
+Function to take in distance data and plot summary showing variability across backgrounds!
+"""
+function plotDistanceSummary(distance_data)
+    pDist = plot()
+    latVals = collect(-20:20)
+    latPlotColors = ColorSchemes.RdBu
+    lineColors = [latPlotColors[i] for i in range(0, 1, length=41)]
+    for (bgIdx,bg) in enumerate([1, 4, 6, 8, 10, 13, 14, 16, 17, 19])
+        for (latIdx, lat) in enumerate(latVals)
+	    if bgIdx == 10
+	        scatter!(pDist,
+		         [bg],
+			[distance_data[latIdx, bgIdx]], 
+                         marker=(:circle, 3, lineColors[latIdx]), 
+                         markerstrokewidth=0,
+                         label="Lat = $(lat)"
+                         )
+            else
+                scatter!(pDist,
+                         [bg],
+                         [distance_data[latIdx, bgIdx]], 
+                         marker=(:circle, 3, lineColors[latIdx]), 
+                         markerstrokewidth=0,
+                         label=""
+                         )
+            end
+        end
+    end
+    
+     plot!(size=(800, 600))
+     plot!(xticks=([1, 4, 6, 8, 10, 13, 14, 16, 17, 19], string.([1, 4, 6, 8, 10, 13, 14, 16, 17, 19])))
+    plot!(xlabel= "Background run ID")
+    plot!(ylabel= "avg(dist_u + dist_n)")
+    # convert to plotly figure
+    dataFig = Plots.plotly_series(pDist)
+    layoutFig = Plots.plotly_layout(pDist)
+
+    
+    # fix overlapping of axis title and tick labels
+    layoutFig[:xaxis][:standoff] = 20
+    layoutFig[:yaxis][:standoff] = 20
+
+   # layoutFig[:title] = Dict{Symbol, Any}(:text=>titleText, :x=>0.5, :xanchor=>"center", :xref=>"paper")
+    
+    layoutFig[:margin] = Dict{Symbol, Any}(:l => 40, :b=> 40, :r=>0, :t=>30) 
+    figure = (data = dataFig, layout = layoutFig)
+    return figure
+end
                    
 # Let's make the app :)
 
@@ -178,49 +323,104 @@ dropdown_options = [Dict("label" => string.(i), "value" => i) for i in collect(-
 bg_options = [Dict("label" => string.(i), "value" => i) for i in [1, 4, 6, 8, 10, 13, 14, 16, 17, 19]]
 
 
-app=dash(external_stylesheets=["https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"])
+dist_data = load("bg_2154_distances.jld", "summed_dist")
+runsToKeep = load("restart_shl_CR2154.jld", "runsToKeep")
+UrOptRMSE, NpOptRMSE, BzOptRMSE = loadRMSEData("restart_shl_CR2154.jld")
 
+explanatory_text = "
+#### Overview:
+
+- QoIs at different latitudes from -20 through 20 are marked in the plot below.
+
+- Scrolling while hovering on the legend entries reveals the full legend.
+
+- Clicking any one or more of the legend entries toggles visibility of the corresponding trace.
+
+- We can zoom in using supplied controls to focus on area before map time.
+
+"
+
+
+markdown_text = "
+#### All backgrounds
+Below we plot the average of `dist_u` and `dist_n` for all backgrounds, these values being colored by latitude as indicated by the legend. Run 4 has the smallest overall distances, and distances seem to drop with decrease in latitude. Distances are calculated using 7 day window before map time.
+"
+
+restart_markdown = "
+#### Restart SHL data
+Next we can view the penalized shifted RMSE for different latitudes and longitudes, for the restart runs. Earth is marked in blue, and we can locate a particular lat and lon by moving the sliders (green circle on plot). These are old restart data (Orientation not changing).
+
+The sliders below control the run we are viewing and the highlighted latitude and longitude. However, over here it seems that the lower RMSEs in general are observed at positive latitudes (opposite of what we saw in the backgrounds). 
+" 
+
+# app=dash(external_stylesheets=["https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"])
+
+app=dash()
 app.title = "SHL Background plots for CR2154"
 
 app.layout = html_div() do
-        html_h1("SHL Background Data",
-                style=(textAlign="center",
-                       )
-                ),
-    
-    
-        # html_label("Slider"),
-        # dcc_slider(
-        # id = "slider1",
-        # min = 0,
-        # max = 9,
-        # marks = Dict([i => (i == 1 ? "Label $(i)" : "$(i)") for i = 1:9]),
-        # value = 5,
-        # # type  = "number"
-    # ),
-    html_div(children=[
-             html_label("Select Latitudes"),
+    html_h1("SHL Background Data", style=(textAlign="center")),
+    dcc_markdown(explanatory_text),
+    html_div(children=[html_label("Select Latitudes"),
         dcc_checklist(id="Latitudes", options = dropdown_options, value=[-4, 7]),
         html_label("Select Background"),
-             dcc_radioitems(id="Background runs", options= bg_options, value=[4]),
-    ],
-            style = Dict("columnCount" => 2)), 
-    html_br(),
-    html_div(style=Dict("columnCount"=>2),
+        dcc_radioitems(id="Background runs", options= bg_options, value=[4])],
+        style = Dict("columnCount" => 2)),
+    html_div(
              children=[
-             #children=[
-             #    html_h1("Plot of QoIs at different latitudes"),
-                           dcc_graph(id="UrPlot",
-                                       figure=makeLatPlots("./all_bg_shl_CR2154.jld"; bgRun=4),
-                                     #style=(display="inline-block")
-                                     ),
-                             dcc_graph(id="NpPlot",
-                                       figure=makeLatPlots("./all_bg_shl_CR2154.jld"; bgRun=4, qoi="Np", ylims=(0, 70)),
-                                      # style=(display="inline-block")
-                                       )
+                 dcc_graph(id="UrPlot",
+                           figure=makeLatPlots("./all_bg_shl_CR2154.jld"; bgRun=4), style=Dict("width"=>"48%", "display"=>"inline-block")),
+                 dcc_graph(id="NpPlot",
+                           figure=makeLatPlots("./all_bg_shl_CR2154.jld"; bgRun=4, qoi="Np", ylims=(0, 70)), style=Dict("width"=>"48%", "display"=>"inline-block"))
             ]
-    )
-    end
+             ),
+    dcc_markdown(markdown_text),
+    dcc_graph(id="Distance Summary",
+              figure=plotDistanceSummary(dist_data)
+              ),
+    dcc_markdown(restart_markdown),
+    html_label("Select Plot Idx"),
+    dcc_slider(id="Plot Idx Slider",
+        min=1,
+        max=132,
+        step=1,
+       value=1,
+       marks=Dict([i=>string.(i) for i in 1:132]) 
+ #       marks=nothing,
+ #       tooltip=Dict(:placement=>"bottom", :always_visible=>true)
+               ),
+    html_label("Select Latitudes"),
+    dcc_slider(id="Latitude Slider",
+        min=-10,
+        max=20,
+        step=2,
+        value=-4,
+        marks=Dict([i => string.(i) for i in collect(-10:2:20)])       
+               ),
+    html_label("Select Longitudes"),
+    dcc_slider(id="Longitude Slider",
+        min=170,
+        max=190,
+        step=2,
+        value=180,
+        marks=Dict([i => string.(i) for i in collect(170:2:190)])       
+    ),
+    html_div(# style=Dict("columnCount"=>3),
+             children=[
+                 dcc_graph(id="UrHeatmap",
+                           figure=plotLatLonHeatmap(sort([collect(-10:2:20); 7]), collect(170:2:190), UrOptRMSE, 1; qoi = "Ur", clims=(0, 350), palette=:YlOrRd_9, colorbar=true),
+                           style=Dict("width"=>"32%", "display"=>"inline-block")
+                           ),
+                 dcc_graph(id="NpHeatmap",
+                           figure=plotLatLonHeatmap(sort([collect(-10:2:20); 7]), collect(170:2:190), NpOptRMSE, 1; qoi = "Np", clims=(0, 35), palette=:YlOrRd_9, colorbar=true),
+                           style=Dict("width"=>"32%", "display"=>"inline-block")
+                           ),
+                 dcc_graph(id="BzHeatmap",
+                           figure=plotLatLonHeatmap(sort([collect(-10:2:20); 7]), collect(170:2:190), BzOptRMSE, 1; qoi = "Bz", clims=(0, 20), palette=:YlOrRd_9, colorbar=true),
+                           style=Dict("width"=>"32%", "display"=>"inline-block")
+                           )
+             ])            
+end
 
 callback!(app, Output("UrPlot", "figure"),
           Input("Latitudes", "value"),
@@ -235,20 +435,37 @@ callback!(app, Output("NpPlot", "figure"),
           ) do selectedLats, selectedBackground
               makeLatPlots("./all_bg_shl_CR2154.jld"; bgRun=selectedBackground, highlightLats = selectedLats, qoi="Np", ylims=(0, 70))
           end           
-# callback!(makePlot, app, CallbackId([], [(Symbol(:slider1), :value)])
 
-# callback!(makePlot,
-#           app,
-#           Output("theplot", "figure"),
-#           Input("slider1", "value"),
-#           )
 
-                     
-# function to make plots given a set of latitudes to highlight and obs
+callback!(app, Output("UrHeatmap", "figure"),
+          Input("Plot Idx Slider", "value"),
+          Input("Latitude Slider", "value"),
+          Input("Longitude Slider", "value"),
+          ) do selectedPlot, selectedLat, selectedLon
+             plotLatLonHeatmap(sort([collect(-10:2:20); 7]), collect(170:2:190), UrOptRMSE, selectedPlot; lat=selectedLat, lon=selectedLon, qoi = "Ur", clims=(0, 350), palette=:YlOrRd_9, colorbar=true)
+             end
 
-# callback!(app, Output("power", "figure"), Input("slider", "value")) do value
-#     powplot(value)
-# end
+callback!(app, Output("NpHeatmap", "figure"),
+          Input("Plot Idx Slider", "value"),
+          Input("Latitude Slider", "value"),
+          Input("Longitude Slider", "value"),
+          ) do selectedPlot, selectedLat, selectedLon
+             plotLatLonHeatmap(sort([collect(-10:2:20); 7]), collect(170:2:190), NpOptRMSE, selectedPlot; lat=selectedLat, lon=selectedLon, qoi = "Np", clims=(0, 35), palette=:YlOrRd_9, colorbar=true)
+             end
+
+
+callback!(app, Output("BzHeatmap", "figure"),
+          Input("Plot Idx Slider", "value"),
+          Input("Latitude Slider", "value"),
+          Input("Longitude Slider", "value"),
+          ) do selectedPlot, selectedLat, selectedLon
+             plotLatLonHeatmap(sort([collect(-10:2:20); 7]), collect(170:2:190), BzOptRMSE, selectedPlot; lat=selectedLat, lon=selectedLon, qoi = "Bz", clims=(0, 20), palette=:YlOrRd_9, colorbar=true)
+             end
 # port = something(tryparse(Int, get(ARGS, 1, "")), tryparse(Int, get(ENV, "PORT", "")), 8080)
 
 # run_server(app, "0.0.0.0", port)
+
+
+## TO DO: Add markdown description of app before plots, including controls etc,
+# add heatmaps of RE-computed RMSE for shl_data, and place them below the QoI plots.
+# Save a static HTML version of the page as well!
